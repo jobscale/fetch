69a9b90b21156c06b4f5a32a270a35d71bcbfdcc
jobscale <jobscale@users.noreply.github.com>
2025-11-23 21:34:52 +0900

    check exists certs added

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ import { ProxyAgent } from 'undici';
 
 const proxy = process.env.HTTPS_PROXY || process.env.https_proxy
 || process.env.HTTP_PROXY || process.env.http_proxy;
-const dispatcher = proxy ? new ProxyAgent(proxy) : undefined;
+const dispatcher = proxy && new ProxyAgent(proxy);
 const certificate = {
-  cert: fs.readFileSync('client-cert.pem'),
-  key: fs.readFileSync('client-cert.key'),
-  ca: fs.readFileSync('ca-cert.pem'),
+  cert: fs.existsSync('certs/client-cert.pem') && fs.readFileSync('certs/client-cert.pem'),
+  key: fs.existsSync('certs/client-cert.key') && fs.readFileSync('certs/client-cert.key'),
+  ca: fs.existsSync('certs/ca-cert.pem') && fs.readFileSync('certs/ca-cert.pem'),
 };
 
 class Ocean {
