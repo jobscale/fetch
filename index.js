8298cb4410f2decc64d8c4b56d52b6e2028def33
jobscale <jobscale@users.noreply.github.com>
2026-02-11 20:07:10 +0900

    fetch timeout added

--- index.js
+++ index.js
@@ -7,23 +7,44 @@ const operate = {
 };
 if (operate.proxy) {
   if (!operate.proxy.match('://')) operate.proxy = `http://${operate.proxy}`;
-  operate.dispatcher = operate.proxy && new ProxyAgent(operate.proxy);
+  operate.dispatcher = new ProxyAgent(operate.proxy);
+  delete operate.proxy;
 }
-const certificate = {
-  cert: fs.existsSync('certs/client-cert.pem') && fs.readFileSync('certs/client-cert.pem'),
-  key: fs.existsSync('certs/client-cert.key') && fs.readFileSync('certs/client-cert.key'),
-  ca: fs.existsSync('certs/ca-cert.pem') && fs.readFileSync('certs/ca-cert.pem'),
+const certificate = () => {
+  if (!certificate.cache) {
+    certificate.cache = {
+      cert: fs.existsSync('certs/client-cert.pem') && fs.readFileSync('certs/client-cert.pem'),
+      key: fs.existsSync('certs/client-cert.key') && fs.readFileSync('certs/client-cert.key'),
+      ca: fs.existsSync('certs/ca-cert.pem') && fs.readFileSync('certs/ca-cert.pem'),
+    };
+  }
+  return certificate.cache;
 };
 
-class Ocean {
-  async fetch(url, opts = {}, extra = {}) {
-    return fetch(url, {
-      dispatcher: operate.dispatcher,
+export class Fetch {
+  fetch(input, opts = {}) {
+    const { timeout = 6_000, ...init } = opts;
+    const ac = new AbortController();
+    ac.terminate = () => clearTimeout(ac.terminate.tid);
+    ac.terminate.tid = setTimeout(() => ac.abort(), timeout);
+    return fetch(input, { ...init, signal: ac.signal })
+    .finally(() => ac.terminate());
+  }
+
+  async customFetch(url, opts = {}, extra = { certificate }) {
+    if (extra.useCert) {
+      if (typeof extra.certificate === 'function') {
+        operate.certificate = extra.certificate();
+      } else {
+        operate.certificate = extra.certificate;
+      }
+    }
+    return this.fetch(url, {
+      ...operate,
       ...opts,
-      ...extra.useCert ? certificate : {},
     });
   }
 }
 
-const ocean = new Ocean();
-export default ocean.fetch;
+const customFetch = (...args) => new Fetch().customFetch(...args);
+export default customFetch;
